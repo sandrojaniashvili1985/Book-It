d09cd9ecbc212955a3b6ed4dbd85c86e25161837
sandro janiashvili <janiashvili.aleksandre@gmail.com>
2024-04-13 19:39:21 +0300

    fdasfsgf

--- apps/backend/server.ts
+++ apps/backend/server.ts
@@ -11,8 +11,24 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 app.use(morgan("tiny"));
+app.use(
+  cors({
+    origin: "https://book-it-frontend-three.vercel.app",
+    credentials: true,
+  })
+);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://book-it-frontend-three.vercel.app"
+  );
+  // Adjust other CORS headers as needed
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use("/api/hotels/uploads", express.static(__dirname + "\\uploads"));
 
