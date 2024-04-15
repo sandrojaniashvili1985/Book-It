2b41c2af370a2035e9ed16c7277e6a564e9a8adb
sandro janiashvili <janiashvili.aleksandre@gmail.com>
2024-04-16 01:32:57 +0300

    sdf

--- apps/frontend/src/pages/BookingPage.tsx
+++ apps/frontend/src/pages/BookingPage.tsx
@@ -25,6 +25,9 @@ const BookingPage = () => {
     fetchBooking();
   }, []);
 
+  console.log("booking", booking);
+  console.log("singlePlace", singlePlace);
+
   return (
     <div className=" flex gap-2 flex-1">
       <div className=" m-4 max-w-sm">
@@ -36,36 +39,30 @@ const BookingPage = () => {
         )}
       </div>
       {booking ? (
-        booking.map((booking) => {
-          <div className="">
-            <h1 className="rubik text-3xl font-semibold my-4">
-              Booking Details
+        <div className="">
+          <h1 className="rubik text-3xl font-semibold my-4">Booking Details</h1>
+          <div className="m-4">
+            <h1 className="text-xl font-semibold inline-block mr-2">
+              Check In:
             </h1>
-            <div className="m-4">
-              <h1 className="text-xl font-semibold inline-block mr-2">
-                Check In:
-              </h1>
-              <span>{booking?.checkIn.split("T")}</span>
-            </div>
-            <div className="m-4">
-              <h1 className="text-xl font-semibold inline-block mr-2">
-                Check Out:
-              </h1>
-              <span>{booking?.checkOut.split("T")}</span>
-            </div>
-            <div className="m-4">
-              <h1 className="text-xl font-semibold">
-                Days of Stay: {booking?.daysOfStay}
-              </h1>
-            </div>
-            <div className="m-4">
-              <h1 className="text-xl font-semibold inline-block mr-2">
-                Amount:
-              </h1>
-              <span>₪{booking?.amount}</span>
-            </div>
-          </div>;
-        })
+            <span>{booking[0]?.checkIn.split("T")[0]}</span>
+          </div>
+          <div className="m-4">
+            <h1 className="text-xl font-semibold inline-block mr-2">
+              Check Out:
+            </h1>
+            <span>{booking[0]?.checkOut.split("T")[0]}</span>
+          </div>
+          <div className="m-4">
+            <h1 className="text-xl font-semibold">
+              Days of Stay: {booking[0]?.daysOfStay}
+            </h1>
+          </div>
+          <div className="m-4">
+            <h1 className="text-xl font-semibold inline-block mr-2">Amount:</h1>
+            <span>₪{booking[0]?.amount}</span>
+          </div>
+        </div>
       ) : (
         <div> No booking found</div>
       )}
